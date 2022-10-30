767aa5b737d2243dff6091ca94a0651fbcc7c89f
BarakMaron <barak062@gmail.com>
2022-10-30 12:01:00 +0200

    feat: adds rental database

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -9,6 +9,14 @@ import Users from './Users.js';
 import Blogs from './Blogs.js';
 import Projects from './Projects.js';
 import ProjectsImages from './ProjectsImages.js';
+import RentalProducts from './RentProducts.js';
+import SaleProducts from './SaleProducts.js';
+import ServiceBook from './ServiceBook.js';
+import ServiceReports from './ServiceReports.js';
+import PartsChanged from './PartsChanged.js';
+import RentalAgreement from './RentalAgreement.js';
+import RentalAgreementList from './RentalAgreementList.js';
+import Customers from './Customers.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -24,10 +32,18 @@ export const sequelize = new Sequelize(
 
 export const CategoriesModel = Categories(sequelize, Sequelize.DataTypes);
 export const ProductsModel = Products(sequelize, Sequelize.DataTypes);
+export const RentalProductsModel = RentalProducts(sequelize, Sequelize.DataTypes);
+export const SaleProductsModel = SaleProducts(sequelize, Sequelize.DataTypes);
 export const ProductsImagesModel = ProductsImages(sequelize, Sequelize.DataTypes);
 export const ProductPropsModel = ProductProps(sequelize, Sequelize.DataTypes);
 export const ProductPartsDiagramModel = ProductPartsDiagram(sequelize, Sequelize.DataTypes);
 export const SparePartsModel = SpareParts(sequelize, Sequelize.DataTypes);
+export const ServiceBookModel = ServiceBook(sequelize, Sequelize.DataTypes);
+export const ServiceReportsModel = ServiceReports(sequelize, Sequelize.DataTypes);
+export const PartsChangedModel = PartsChanged(sequelize, Sequelize.DataTypes);
+export const RentalAgreementModel = RentalAgreement(sequelize, Sequelize.DataTypes);
+export const RentalAgreementListModel = RentalAgreementList(sequelize, Sequelize.DataTypes);
+export const CustomersModel = Customers(sequelize, Sequelize.DataTypes);
 export const UsersModel = Users(sequelize, Sequelize.DataTypes);
 export const BlogsModel = Blogs(sequelize, Sequelize.DataTypes);
 export const ProjectsModel = Projects(sequelize, Sequelize.DataTypes);
@@ -35,6 +51,12 @@ export const ProjectsImagesModel = ProjectsImages(sequelize, Sequelize.DataTypes
 
 CategoriesModel.hasMany(ProductsModel);
 ProductsModel.belongsTo(CategoriesModel);
+
+ProductsModel.hasOne(RentalProductsModel);
+RentalProductsModel.belongsTo(ProductsModel);
+
+ProductsModel.hasOne(SaleProductsModel);
+SaleProductsModel.belongsTo(ProductsModel);
 
 ProductsModel.hasMany(ProductsImagesModel);
 ProductsImagesModel.belongsTo(ProductsModel);
@@ -50,3 +72,22 @@ SparePartsModel.belongsTo(ProductPartsDiagramModel);
 
 ProjectsModel.hasMany(ProjectsImagesModel);
 ProjectsImagesModel.belongsTo(ProjectsModel);
+
+CustomersModel.hasMany(RentalAgreementModel);
+RentalAgreementModel.belongsTo(CustomersModel);
+
+RentalAgreementModel.hasOne(RentalAgreementListModel);
+RentalAgreementListModel.belongsTo(RentalAgreementModel);
+
+RentalProductsModel.hasOne(ServiceBookModel);
+ServiceBookModel.belongsTo(RentalProductsModel);
+
+ServiceBookModel.hasMany(ServiceReportsModel);
+ServiceReportsModel.belongsTo(ServiceBookModel);
+
+// ServiceReportsModel.hasMany(SparePartsModel, {
+//   through: PartsChangedModel
+// });
+SparePartsModel.belongsToMany(ServiceReportsModel, {
+  through: PartsChangedModel
+});
