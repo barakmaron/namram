39225f9a21c30dccb7378fe39e15502945353313
BarakMaron <barak062@gmail.com>
2022-11-04 12:36:11 +0200

    feat: adds monday ui core css

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import AppConnector from './AppConnector';
+import "monday-ui-react-core/dist/main.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
