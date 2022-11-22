38e8eaba46f0b35114334bfb406410f6497ab09e
BarakMaron <barak062@gmail.com>
2022-11-22 14:06:56 +0200

    feat: adds errors to image upload middleware

--- server/middleware/UploadImageMiddleware.js
+++ server/middleware/UploadImageMiddleware.js
@@ -8,9 +8,8 @@ const __dirname = path.dirname(__filename);
 const UploadMiddleware = multer({
     dest: path.join(__dirname, "..//Images"),
     onError : function(err, next) {
-        console.log('error', err);
         next(err);
-      }
+    }
 });
 
 export default UploadMiddleware;
