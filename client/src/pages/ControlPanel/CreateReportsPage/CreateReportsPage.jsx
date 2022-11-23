a6cf8241c8a720662a764b85a5ca87e2e885c8e2
BarakMaron <barak062@gmail.com>
2022-11-23 15:03:18 +0200

    feat: adds title to page

--- client/src/pages/ControlPanel/CreateReportsPage/CreateReportsPage.jsx
+++ client/src/pages/ControlPanel/CreateReportsPage/CreateReportsPage.jsx
@@ -6,6 +6,9 @@ const CreateReportsPage = () => {
 
   return <>
     <div className='flex mx-auto flex-col flex-wrap gap-12 justify-center w-fit'>
+    <h2 className="w-fit mx-auto my-4 text-4xl font-bold text-green-600">
+      הפקת דוחות
+    </h2>
       <SearchRentalAgreements/>
       <IncomeReportConnector />
     </div>
