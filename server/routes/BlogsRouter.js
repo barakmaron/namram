93709b82eb9409fbb98b7d5ffe2b06cd48b99cd1
BarakMaron <barak062@gmail.com>
2022-11-28 12:51:59 +0200

    feat: add user login validation to rouets:

--- server/routes/BlogsRouter.js
+++ server/routes/BlogsRouter.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import BlogsController from '../controllers/BlogsController.js';
+import AuthenticateToken from '../middleware/AuthMiddleware.js';
 import UploadMiddleware from '../middleware/UploadImageMiddleware.js';
 
 const router = express.Router();
 
 router.get('/', BlogsController.GetAllBlogs);
-router.post('/', UploadMiddleware.single('Image'), BlogsController.AddBlog);
-router.patch('/:id', BlogsController.PatchBlog);
-router.delete('/:id', BlogsController.DeleteBlog);
+router.post('/', AuthenticateToken, UploadMiddleware.single('Image'), BlogsController.AddBlog);
+router.patch('/:id', AuthenticateToken, BlogsController.PatchBlog);
+router.delete('/:id', AuthenticateToken, BlogsController.DeleteBlog);
 
 export default router;
--- server/routes/ProjectsRoutes/ProjectsRouter.js
+++ server/routes/ProjectsRoutes/ProjectsRouter.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import ProjectsController from '../../controllers/Projects/ProjectsController.js';
+import AuthenticateToken from '../../middleware/AuthMiddleware.js';
 import UploadMiddleware from '../../middleware/UploadImageMiddleware.js';
 import ProjectsImagesRouter from './ProjectsImagesRouter.js';
 
 const router = express.Router();
 
 router.get('/', ProjectsController.GetAllProjects);
-router.post('/', UploadMiddleware.array('Image'), ProjectsController.AddProject);
-router.delete('/:id', ProjectsController.DeleteProject);
-router.patch('/:id', ProjectsController.PatchProject);
-router.use('/:id/images', ProjectsImagesRouter);
+router.post('/', AuthenticateToken, UploadMiddleware.array('Image'), ProjectsController.AddProject);
+router.delete('/:id', AuthenticateToken, ProjectsController.DeleteProject);
+router.patch('/:id', AuthenticateToken, ProjectsController.PatchProject);
+router.use('/:id/images', AuthenticateToken, ProjectsImagesRouter);
 
 export default router;
