2a0514dbef4b3fd2b04db259417cd041b934693f
BarakMaron <barak062@gmail.com>
2022-11-28 14:32:54 +0200

    feat: adds projects requests schemas and validation

--- server/routes/ProjectsRoutes/ProjectsRouter.js
+++ server/routes/ProjectsRoutes/ProjectsRouter.js
@@ -2,14 +2,31 @@ import express from 'express';
 import ProjectsController from '../../controllers/Projects/ProjectsController.js';
 import AuthenticateToken from '../../middleware/AuthMiddleware.js';
 import UploadMiddleware from '../../middleware/UploadImageMiddleware.js';
+import { validate } from '../../middleware/ValidationErrorMiddleware.js';
 import ProjectsImagesRouter from './ProjectsImagesRouter.js';
+import { checkSchema } from 'express-validator';
+import ProjectsSchemas from '../../validationSchemas/ProjectsSchemas/ProjectsSchemas.js';
 
 const router = express.Router();
 
 router.get('/', ProjectsController.GetAllProjects);
-router.post('/', AuthenticateToken, UploadMiddleware.array('Image'), ProjectsController.AddProject);
-router.delete('/:id', AuthenticateToken, ProjectsController.DeleteProject);
-router.patch('/:id', AuthenticateToken, ProjectsController.PatchProject);
+
+router.post('/', 
+    AuthenticateToken, 
+    UploadMiddleware.array('Image'),
+    validate(checkSchema(ProjectsSchemas.AddProject)), 
+    ProjectsController.AddProject);
+
+router.delete('/:id', 
+    AuthenticateToken, 
+    validate(checkSchema(ProjectsSchemas.DeleteProject)), 
+    ProjectsController.DeleteProject);
+
+router.patch('/:id', 
+    AuthenticateToken, 
+    validate(checkSchema(ProjectsSchemas.PatchProject)), 
+    ProjectsController.PatchProject);
+
 router.use('/:id/images', AuthenticateToken, ProjectsImagesRouter);
 
 export default router;
