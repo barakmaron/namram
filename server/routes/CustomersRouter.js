6c424e831ead27ebdbc0f1037104d60d16b62d97
BarakMaron <barak062@gmail.com>
2022-11-28 15:05:43 +0200

    feat: adds categories requests schemas and validation

--- server/routes/CustomersRouter.js
+++ server/routes/CustomersRouter.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import CustomersController from '../controllers/CustomersController.js';
+import { validate } from '../middleware/ValidationErrorMiddleware.js';
+import { checkSchema } from 'express-validator';
+import CustomersSchemas from '../validationSchemas/CustomersSchemas.js';
 
 const router = express.Router();
 
 router.get('/', CustomersController.GetAllCustomers);
-router.patch('/:id', CustomersController.PatchCustomer);
-router.delete('/:id', CustomersController.DeleteCustomer);
+
+router.patch('/:id', 
+validate(checkSchema(CustomersSchemas.PatchCustomer)),
+CustomersController.PatchCustomer);
+
+router.delete('/:id', 
+validate(checkSchema(CustomersSchemas.DeleteCustomer)),
+CustomersController.DeleteCustomer);
 
 export default router;
