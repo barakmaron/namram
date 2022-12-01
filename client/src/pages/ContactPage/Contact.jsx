44be8de3612a7195630de17018d1e2b410da282f
BarakMaron <barak062@gmail.com>
2022-12-01 14:47:00 +0200

    feat: adds contact form error handler

--- client/src/pages/ContactPage/Contact.jsx
+++ client/src/pages/ContactPage/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react';
 import ContactInfo from '../../components/ContactInfo/ContactInfo';
-import Form from '../../components/Form/Form';
+import FormConnector from '../../components/Form/FormConnector';
 import Map from '../../components/Map/Map';
 import Constants from '../../Constants';
 import contact_inputs from './FormConstantans';
@@ -21,7 +21,7 @@ const Contact = ({
     <div className='py-10 mx-auto flex flex-col sm:flex-row items-start justify-center gap-5' dir='rtl'>
         <div className='border-l-2 border-green-600 border-solid px-4 flex flex-col items-start'>
             <h3 className=' text-slate-700 font-bold text-3xl'>טופס צור קשר</h3>
-            <Form inputs={contact_inputs} action={send_form} />
+            <FormConnector inputs={contact_inputs} action={send_form} />
         </div>
         <div className=' flex flex-col items-start h-full justify-start'>
             <h3 className=' text-slate-700 font-bold text-3xl'>פרטים ליצרת קשר</h3>
