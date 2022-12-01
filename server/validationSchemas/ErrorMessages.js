71d7af7d5ad19cc5b3491d045ecdebc95ce667ed
BarakMaron <barak062@gmail.com>
2022-12-01 14:44:21 +0200

    fix: contact schema validtion

---
+++ server/validationSchemas/ErrorMessages.js
@@ -0,0 +1,10 @@
+
+const ErrorMessages = {
+    text_empty: "טקסט לא יכול להיות ריק",
+    phone: "אנא הכנס מספר טלפון תקין",
+    email: "אנא הכנס איימל תקין",
+    full_name: "אנא הכנס שם מלא",
+    company_name: "אנא הכנס שם חברה תקין"
+};
+
+export default ErrorMessages;
