e37a7e5c0d3beb7048ea50c5ce59b2575be4fcb0
BarakMaron <barak062@gmail.com>
2022-12-01 14:45:02 +0200

    feat: adds api request handler

--- client/src/redux/actions/ContactFormActions.js
+++ client/src/redux/actions/ContactFormActions.js
@@ -1,13 +1,15 @@
+import ApiMessagesConstants from "../../ApiMessagesConstants";
 import Constants from "../../Constants"
 import SendApiRequest from "../../services/ApiService"
+import { Successful, DispatchError } from "./ApiHandlerActions";
 
 export const SendContactForm = (form) => {
     return async (dispatch) => {
         try {
-            const contact = await SendApiRequest(`/contact`, Constants.API_METHODS.POST, form);
-            
+            await SendApiRequest(`/contact`, Constants.API_METHODS.POST, form);
+            dispatch(Successful(ApiMessagesConstants.contact.successful));
         } catch (err) {
-
+            DispatchError(dispatch, err, ApiMessagesConstants.contact.failed)
         }
     }
 }
