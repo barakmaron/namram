03760a91bb84317ac303136ab5df1df8ec416517
BarakMaron <barak062@gmail.com>
2022-12-01 14:46:20 +0200

    feat: adds successful and error components

---
+++ client/src/ApiMessagesConstants.js
@@ -0,0 +1,10 @@
+
+const ApiMessagesConstants = {
+    server_error: "אנו מתנצלים אך לא הצלחנו לבצע את הפועולה אחרונה אנא נסה שוב מאוחר יותר",
+    contact: {
+        successful: "טופס צור קשר נשלח בהצלחה",
+        failed: "לא הצלחנו לשלוח טופס צור קשר"
+    }
+};
+
+export default ApiMessagesConstants;
