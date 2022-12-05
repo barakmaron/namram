4dfbb790d180bb5e8ed6975a0301664ada06b8d5
BarakMaron <barak062@gmail.com>
2022-12-05 16:30:08 +0200

    feat: adds error handling on server

--- server/controllers/ImagesController.js
+++ server/controllers/ImagesController.js
@@ -1,16 +1,17 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { StatusCode } from 'status-code-enum';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-async function GetImage(req, res) {
+async function GetImage(req, res, next) {
     try {
         const { file_name } = req.params;   
         res.setHeader('content-type', "image/jpeg");     
         return res.sendFile(path.join(__dirname, `../Images/${file_name}`));
     } catch (err) {
-        throw err;
+        next(err);
     }
 }
 
