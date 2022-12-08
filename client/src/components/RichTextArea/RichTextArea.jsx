a02b155d80ddc7185b9c5dbe11af44c4b0413bb2
BarakMaron <barak062@gmail.com>
2022-12-08 15:17:23 +0200

    feat: adds rich text area to forms

--- client/src/components/RichTextArea/RichTextArea.jsx
+++ client/src/components/RichTextArea/RichTextArea.jsx
@@ -1,41 +1,6 @@
 import React from 'react'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-
-const CustomToolbar = ({
-  id
-}) => (
-    <>
-    { id && <div id={`toolbar_${id}`}>
-      <select className="ql-header" defaultValue={""} onChange={e => e.persist()}>
-        <option value="2" />
-        <option value="3" />
-        <option value="4" />
-        <option selected />
-      </select>
-      <button className="ql-bold" />
-      <button className="ql-italic" />
-      <button className='ql-list' value="ordered" />
-      <button className='ql-list' value="bullet" />
-      <select className="ql-color">
-        <option value="red" />
-        <option value="green" />
-        <option value="blue" />
-        <option value="orange" />
-        <option value="violet" />
-        <option value="#d0d1d2" />
-        <option selected />
-      </select>
-      <select className='ql-align'>
-        <option value="right" />
-        <option value="center" />
-        <option value="justify" />
-      </select>
-      <button className='ql-direction' value="rtl" />
-    </div>}
-    </>
-  );
-
   
   const RichTextArea = ({
     value,
