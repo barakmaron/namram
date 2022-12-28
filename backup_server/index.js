3915f6002850743db09e5dc41fc17fd9f4afdf62
BarakMaron <barak062@gmail.com>
2022-12-28 14:17:44 +0200

    feat: adds console logs

--- backup_server/index.js
+++ backup_server/index.js
@@ -8,12 +8,24 @@ dotenv.config();
 
 async function main(){
     try {
+        console.log("Trying to login");
         await Login();
+        console.log("Logged in");
+        console.log("Trying to create dir");
         const folder = await FileManager.CreateDir();
+        console.log("Dir created");
+        console.log("Trying to get DataBase");
         const database = await GetDatabase();
+        console.log("Got database");
+        console.log("Trying to save database");
         FileManager.CreateFile("database.sql", folder, database);
+        console.log("Database saved");
+        console.log("Trying to get rental agreements");
         const rental_agreements = await GetRentalAgreements();
+        console.log("Got rental agreements");
+        console.log("Trying to save rental agreements");
         FileManager.CreateFile("rental_agreements.html", folder, rental_agreements);
+        console.log("Rental agreements saved");
     } catch (err) {
         const date = moment().format('DD_MM_YYYY');
         FileManager.CreateFile(`error_log_${date}.txt`, './', err.toString());
