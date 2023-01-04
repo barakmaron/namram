6a6bfd0e0bc04a781a9b9252c09a846f38e768b2
BarakMaron <barak062@gmail.com>
2023-01-04 20:27:14 +0200

    feat: adds pdfs as diagrams

--- server/controllers/ImagesController.js
+++ server/controllers/ImagesController.js
@@ -8,7 +8,10 @@ const __dirname = path.dirname(__filename);
 async function GetImage(req, res, next) {
     try {
         const { file_name } = req.params;   
-        res.setHeader('content-type', "image/jpeg");     
+        if(file_name.includes(".webp"))
+            res.setHeader('content-type', "image/jpeg");     
+        else
+            res.setHeader('content-type', 'application/pdf');
         return res.sendFile(path.join(__dirname, `../Images/${file_name}`));
     } catch (err) {
         next(err);
