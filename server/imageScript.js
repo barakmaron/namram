f08e0ee78875049b8c0275db8ea2afd716eb97e8
BarakMaron <barak062@gmail.com>
2023-12-09 16:52:54 +0200

    add images

--- server/imageScript.js
+++ server/imageScript.js
@@ -5,26 +5,28 @@ import fs from 'fs';
 const readdir = promisify(fs.readdir);
 
 async function processWebPImages(folderPath) {
-  try {
-    // Read the list of files in the folder
-    const files = await readdir(folderPath);
+    try {
+        // Read the list of files in the folder
+        const files = await readdir(folderPath);
 
-    // Iterate over each file
-    for (const file of files) {
-      // Read the image file
-      const buffer = fs.readFileSync(`${folderPath}/${file}`);
+        // Iterate over each file
+        for (const file of files) {
+            if (file.split('_').length <= 1 && file.split('.')[1] === 'webp') {
+                // Read the image file
+                const buffer = fs.readFileSync(`${folderPath}/${file}`);
 
-      // Process the image using sharp
-      await sharp(buffer)
-        .webp({ quality: 100 })
-        .resize({ width: 56, height: 56 })
-        .toFile(`./Images/${file.split('.')[0]}_thumb.webp`);
-        
-      console.log(`Processed image: ${file}`);
+                // Process the image using sharp
+                await sharp(buffer)
+                    .webp({ quality: 100 })
+                    .resize({ width: 56, height: 56 })
+                    .toFile(`./Images/${file.split('.')[0]}_thumb.webp`);
+
+                console.log(`Processed image: ${file}`);
+            }
+        }
+    } catch (error) {
+        console.error('Error processing images:', error);
     }
-  } catch (error) {
-    console.error('Error processing images:', error);
-  }
 }
 
 // Replace 'path/to/your/webp/images/folder' with the actual path to your WebP image folder
